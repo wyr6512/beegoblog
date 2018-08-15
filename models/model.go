8f100e66dfbef60317c2f1198ded7b21394a7cec
wyr6512 <wyr6512@163.com>
2018-08-15 22:18:32 +0800

    table struct

--- models/model.go
+++ models/model.go
@@ -19,14 +19,62 @@ func init() {
 	orm.RunSyncdb("default", false, true)
 }
 
+//分类
 type Category struct {
+	Id           int64
+	Name         string `orm:"size(32)"`
+	Views        int64
+	ArticleCount int64
+	CreatedAt    time.Time `orm:"auto_now_add;type(datetime)"`
+	UpdateAt     time.Time `orm:"auto_now;type(datetime)"`
 }
 
+//文章
 type Article struct {
+	Id            int64
+	Uid           int64
+	Author        string `orm:"size(32)"`
+	Title         string `orm:"size(128)"`
+	Abstract      string
+	CategoryId    int64
+	Views         int64
+	Tags          string
+	ReplyCount    int64
+	ReplyLastTime time.Time `orm:"auto_now;type(datetime)"`
+	ReplyLastUid  int64
+	Status        int       `orm:"default(0)"`
+	CreatedAt     time.Time `orm:"auto_now_add;type(datetime);index"`
+	UpdateAt      time.Time `orm:"auto_now;type(datetime);index"`
 }
 
+//文章内容
+type Content struct {
+	Id        int64
+	ArticleId int64     `orm:"index"`
+	Content   string    `orm:"size(5000)"`
+	CreatedAt time.Time `orm:"auto_now_add;type(datetime);index"`
+	UpdateAt  time.Time `orm:"auto_now;type(datetime);index"`
+}
+
+//评论
 type Comment struct {
+	Id        int64
+	ArticleId int64     `orm:"index"`
+	Name      string    `orm:"size(32)"`
+	Content   string    `orm:"size(512)"`
+	CreatedAt time.Time `orm:"auto_now_add;type(datetime);index"`
 }
 
+//标签
 type Tag struct {
+	Id        int64
+	Name      string    `orm:"size(16)"`
+	CreatedAt time.Time `orm:"auto_now_add;type(datetime);index"`
+}
+
+//文章-标签对应关系
+type ArticleTag struct {
+	Id        int64
+	ArticleId int64 `orm:"index"`
+	TagId     int64 `orm:"index"`
 }
